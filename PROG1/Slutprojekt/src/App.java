a5f21b57c5fd44967dbfbd04323cc3b6c23d648d
BuildTools <unconfigured@null.spigotmc.org>
2021-12-01 16:14:45 +0100

    Skapade klass för hundar, 6.1

---
+++ PROG1/Slutprojekt/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Dog a = new Dog("Namn", "tax", 5, 12);
+        System.out.print(a);
+    }
+}
---
+++ PROG1/Slutprojekt/src/Dog.java
@@ -0,0 +1,63 @@
+public class Dog {
+
+    private String name;
+    private String breed;
+    private int age;
+    private int weight;
+
+    public Dog(String name, String breed, int age, int weight){
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.weight = weight;
+    }
+    
+    public String toString(){
+        return String.format(
+            "Name: %s Age: %s år Breed: %s Weight: %s kg TailLength: %s dm", this.name, this.age, this.breed, this.weight, getTailLength());
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getBreed(){
+        return this.breed;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public int getWeight(){
+        return this.weight;
+    }
+
+    public double getTailLength(){
+        return calculateTailLength();
+    }
+
+    public boolean increaseDogAge(int n){
+        if (n > 0){
+            this.age += n;
+            return true;
+        }
+        else{
+            System.out.println("Error: you can only increase age by a positive integer");
+            return false;
+        }
+    }
+
+    private double calculateTailLength(){
+        final double taxTailLength = 3.7;
+        String[] taxar = {"tax", "dachshund", "mäyräkoira", "teckel"};
+        return (checkInStringArray(taxar, this.breed)) ? taxTailLength : this.age*this.weight/10.0;
+    }
+
+    private boolean checkInStringArray(String[] stringArray, String item){
+        for (String element : stringArray) {
+            if (item.equals(element.toLowerCase())) return true;
+        }
+        return false;
+    }
+}
